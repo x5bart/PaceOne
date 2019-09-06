c6679aba7a8f9a7aa5bcca65d5f86738ec0aa192
Pavel Kovalchuk <p.kovalchuk@bestoptom.ru>
2019-09-06 23:28:20 +0300

    Add null etSpeed fix bags

--- app/src/main/java/com/x5bart_soft/paceone/MainActivity.kt
+++ app/src/main/java/com/x5bart_soft/paceone/MainActivity.kt
@@ -1,26 +1,16 @@
 package com.x5bart_soft.paceone
 
 import android.app.AlertDialog
-import android.app.Dialog
-import android.content.Context
-import android.content.DialogInterface
-import android.graphics.Color
-import android.graphics.ColorSpace
-import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "btnlog"
-    val h1 = 60 //60 min
+
+    private val h1 = 60 //60 min
+    private val km = 1000 //1000 m
+    private val sec = 3.600 // 3600sec/hours
     var res = 0.0
-    val km = 1000 //1000 m
-    val coef = 3.600 // 3600sec/hours
     var num1 = 0
     var num2 = 0
     var num3 = 0.0
@@ -40,13 +30,13 @@ class MainActivity : AppCompatActivity() {
             if (etPaceS.text.toString().isEmpty()) {
                 num2 = 0
                 etPaceS.setText("00")
-            } else
-                num2 = etPaceS.text.toString().toInt()
+            } else num2 = etPaceS.text.toString().toInt()
+            if (etPaceS.text.toString().toInt() < 10) etPaceS.setText("0$num2")
 
             if (num1 == 0 && num2 == 0) showMyDialog()
             else {
                 val minutes = ((num1 * h1) + num2).toDouble()
-                res = (km / minutes) * coef
+                res = (km / minutes) * sec
                 etSpeed.setText(res.toString())
             }
         }
@@ -56,12 +46,13 @@ class MainActivity : AppCompatActivity() {
                 etSpeed.setText("0.0")
             } else {
                 num3 = etSpeed.text.toString().toDouble()
-                res = num3 / coef
+                res = num3 / sec
                 val min = (km / res / h1).toInt()
-                val sec = (((km / res) - (min * h1)) * 100).toInt()
+                val sec = ((km / res) - (min * h1)).toInt()
 
                 etPaceM.setText(min.toString())
-                etPaceS.setText(sec.toString())
+                if (sec < 10) etPaceS.setText("0$sec")
+                else etPaceS.setText(sec.toString())
             }
         }
 
