c294f1ef7a79ed781ee665babfc0a31f50a8511b
Pavel Kovalchuk <p.kovalchuk@bestoptom.ru>
2019-09-06 15:12:09 +0300

    fix fun on NUllable

--- app/src/main/java/com/x5bart_soft/paceone/MainActivity.kt
+++ app/src/main/java/com/x5bart_soft/paceone/MainActivity.kt
@@ -3,6 +3,8 @@ package com.x5bart_soft.paceone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +13,24 @@ class MainActivity : AppCompatActivity() {
     var res = 0.0
     val km = 1000 //1000 m
     val coef = 3.600 // 3600sec/hours
-
+    var num1 = 0
+    var num2 = 0
+    var num3 = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnToSpeed.setOnClickListener {
 
-            val num1 = etPaceM.text.toString().toInt()
-            val num2 = etPaceS.text.toString().toInt()
+            if (etPaceM.text.toString().length == 0)
+                num1 = 0
+            else
+                num1 = etPaceM.text.toString().toInt()
+            if (etPaceS.text.toString().length == 0)
+                num2 = 0
+            else
+                num2 = etPaceS.text.toString().toInt()
+
             val minutes = ((num1 * h1) + num2).toDouble()
             res = (km / minutes) * coef
 
@@ -27,16 +38,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnToPace.setOnClickListener {
-
-            val num1 = etSpeed.text.toString().toDouble()
-            res = num1 / coef
-            val min = (km / res/h1).toInt()
+            if (etSpeed.text.toString().length == 0)
+                num3 = 0.0
+            else
+                num3 = etSpeed.text.toString().toDouble()
+            res = num3 / coef
+            val min = (km / res / h1).toInt()
             val sec = (((km / res) - (min * h1)) * 100).toInt()
-            Log.d(TAG, "res = $res, min = $min sec = $sec")
 
             etPaceM.setText(min.toString())
             etPaceS.setText(sec.toString())
         }
     }
 }
- 
