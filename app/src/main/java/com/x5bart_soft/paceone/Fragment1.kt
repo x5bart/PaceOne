fda93574d70060eb9581d90e4ff4f749400a692a
Pavel Kovalchuk <p.kovalchuk@bestoptom.ru>
2019-09-11 16:08:22 +0300

    code review

--- app/src/main/java/com/x5bart_soft/paceone/Fragment1.kt
+++ app/src/main/java/com/x5bart_soft/paceone/Fragment1.kt
@@ -1,91 +1,34 @@
 package com.x5bart_soft.paceone
 
 
-import android.app.AlertDialog
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment1.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class Fragment1 : Fragment() {
-    private val h1 = 60 //60 min
+    private val hour = 60 //60 min
     private val km = 1000 //1000 m
     private val sec = 3.600 // 3600sec/hours
     var res = 0.0
-    var num1 = 0
-    var num2 = 0
-    var num3 = 0.0
-
-
-
-
-
-//    private fun showMyDialog() {
-//        val dialog: AlertDialog
-//        val builder = AlertDialog.Builder()
-//        builder.setTitle("Error")
-//        builder.setMessage("Please enter values for conversion.")
-//        dialog = builder.create()
-//        dialog.show()
-//    }
-
-    private fun mKmToKmH() {
-        if (etPaceM.text.toString().isEmpty()) {
-            num1 = 0
-            etPaceM.setText("0")
-        } else
-            num1 = etPaceM.text.toString().toInt()
-
-        if (etPaceS.text.toString().isEmpty()) {
-            num2 = 0
-            etPaceS.setText("00")
-        } else num2 = etPaceS.text.toString().toInt()
-        if (etPaceS.text.toString().toInt() < 10) etPaceS.setText("0$num2")
-
-        if (num1 == 0 && num2 == 0)
-        //showMyDialog()
-        else {
-            val minutes = ((num1 * h1) + num2).toDouble()
-            res = (km / minutes) * sec
-            etSpeed.setText(res.toString())
-        }
-    }
-
-    private fun kmHToMKm() {
-        if (etSpeed.text.toString().isEmpty() || etSpeed.text.toString().toDouble() == 0.0) {
-//            showMyDialog()
-            etSpeed.setText("0.0")
-        } else {
-            num3 = etSpeed.text.toString().toDouble()
-            res = num3 / sec
-            val min = (km / res / h1).toInt()
-            val sec = ((km / res) - (min * h1)).toInt()
-
-            etPaceM.setText(min.toString())
-            if (sec < 10) etPaceS.setText("0$sec")
-            else etPaceS.setText(sec.toString())
-        }
-    }
-
+    var etMin = 0
+    var etSec = 0
+    var etKm = 0.0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment1, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        etPaceS.setOnEditorActionListener{
-                _, actionId, _ ->
+
+        etPaceS.setOnEditorActionListener { _, actionId, _ ->
             return@setOnEditorActionListener when (actionId) {
                 EditorInfo.IME_ACTION_GO -> {
                     mKmToKmH()
@@ -95,8 +38,7 @@ class Fragment1 : Fragment() {
             }
         }
 
-        etSpeed.setOnEditorActionListener{
-                _, actionId, _ ->
+        etSpeed.setOnEditorActionListener { _, actionId, _ ->
             return@setOnEditorActionListener when (actionId) {
                 EditorInfo.IME_ACTION_GO -> {
                     kmHToMKm()
@@ -106,11 +48,49 @@ class Fragment1 : Fragment() {
             }
         }
 
-        btnToSpeed.setOnClickListener{
+        btnToSpeed.setOnClickListener {
             mKmToKmH()
         }
-        btnToPace.setOnClickListener{
+        btnToPace.setOnClickListener {
             kmHToMKm()
+        }
+    }
+
+    private fun mKmToKmH() {
+        if (etPaceM.text.toString().isEmpty()) {
+            etMin = 0
+            etPaceM.setText("0")
+        } else
+            etMin = etPaceM.text.toString().toInt()
+
+        if (etPaceS.text.toString().isEmpty()) {
+            etSec = 0
+            etPaceS.setText("00")
+        } else etSec = etPaceS.text.toString().toInt()
+
+        if (etPaceS.text.toString().toInt() < 10) etPaceS.setText("0$etSec")
+
+        if (etMin == 0 && etSec == 0) etSpeed.setText("0.00")
+        else {
+            val second = ((etMin * hour) + etSec).toDouble()
+            res = (km / second) * sec
+            etSpeed.setText(res.toString())
+        }
+
+    }
+
+    private fun kmHToMKm() {
+        if (etSpeed.text.toString().isEmpty() || etSpeed.text.toString().toDouble() == 0.00) {
+            etSpeed.setText("0.00")
+        } else {
+            etKm = etSpeed.text.toString().toDouble()
+            res = etKm / sec
+            val min = (km / res / hour).toInt()
+            val sec = ((km / res) - (min * hour)).toInt()
+
+            etPaceM.setText(min.toString())
+            if (sec < 10) etPaceS.setText("0$sec")
+            else etPaceS.setText(sec.toString())
         }
     }
 }
