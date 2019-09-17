68648e02a3ec86a5a9c6eb54714b7fd9fc5c310e
Pavel Kovalchuk <p.kovalchuk@bestoptom.ru>
2019-09-17 17:19:12 +0300

    REVIEW LAYOUT

--- app/src/main/java/com/x5bart_soft/paceone/Fragment1.kt
+++ app/src/main/java/com/x5bart_soft/paceone/Fragment1.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.fragment1.*
@@ -39,10 +38,6 @@ class Fragment1 : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        calc1.isVisible = false
-        calc2.isVisible = false
-        btnCalc.isVisible = false
-        tvCalcRes.isVisible = false
 
         etPaceM.setOnFocusChangeListener { view, hasFocus ->
             flagListener = 1
@@ -59,11 +54,36 @@ class Fragment1 : Fragment() {
             Log.d(TAG, "flag  = $flagListener")
             etSpeed.selectAll()
         }
+        etCalcDistH.setOnFocusChangeListener { view, hasFocus ->
+            flagListener = 4
+            Log.d(TAG, "flag  = $flagListener")
+            etSpeed.selectAll()
+        }
+        etCalcDistM.setOnFocusChangeListener { view, hasFocus ->
+            flagListener = 5
+            Log.d(TAG, "flag  = $flagListener")
+            etSpeed.selectAll()
+        }
+        etCalcDistS.setOnFocusChangeListener { view, hasFocus ->
+            flagListener = 6
+            Log.d(TAG, "flag  = $flagListener")
+            etSpeed.selectAll()
+        }
+        etCalcKmh.setOnFocusChangeListener { view, hasFocus ->
+            flagListener = 7
+            Log.d(TAG, "flag  = $flagListener")
+            etSpeed.selectAll()
+        }
 
         etPaceM.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
 
-                if (flagListener == 1) mKmToKmH()
+                if (!cbFixDist.isChecked && flagListener == 1) {
+                    mKmToKmH()
+                    dist()
+                    time()
+                }
+                if (cbFixDist.isChecked && flagListener == 1) mKmToKmH()
             }
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -72,10 +92,14 @@ class Fragment1 : Fragment() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
         })
-
         etPaceS.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
-                if (flagListener == 2) mKmToKmH()
+                if (!cbFixDist.isChecked && flagListener == 2) {
+                    mKmToKmH()
+                    dist()
+                    time()
+                }
+                if (cbFixDist.isChecked && flagListener == 2) mKmToKmH()
             }
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -84,10 +108,14 @@ class Fragment1 : Fragment() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
         })
-
         etSpeed.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
-                if (flagListener == 3) kmHToMKm()
+                if (!cbFixDist.isChecked && flagListener == 3) {
+                    mKmToKmH()
+                    dist()
+                    time()
+                }
+                if (cbFixDist.isChecked && flagListener == 3) mKmToKmH()
             }
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -96,65 +124,54 @@ class Fragment1 : Fragment() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
         })
+        etCalcDistH.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (cbFixDist.isChecked && flagListener == 4) speed()
+                if (flagListener == 4) dist()
+            }
 
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
-//        etPaceS.setOnEditorActionListener { _, actionId, _ ->
-//            return@setOnEditorActionListener when (actionId) {
-//                EditorInfo.IME_ACTION_GO -> {
-//                    mKmToKmH()
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-//
-//        etSpeed.setOnEditorActionListener { _, actionId, _ ->
-//            return@setOnEditorActionListener when (actionId) {
-//                EditorInfo.IME_ACTION_GO -> {
-//                    kmHToMKm()
-//                    true
-//                }
-//                else -> false
-//            }
-//        }
-//
-//        etCalcDistS.setOnEditorActionListener { _, actionId, _ ->
-//            return@setOnEditorActionListener when (actionId) {
-//                EditorInfo.IME_ACTION_NEXT -> {
-//                    if (calcDist.isChecked)
-//                        dist()
-//                    true
-//
-//                }
-//                else -> false
-//
-//            }
-//        }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+        etCalcDistM.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (cbFixDist.isChecked && flagListener == 5) speed()
+                if (!cbFixDist.isChecked && flagListener == 5) dist()
+            }
 
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
-        btnCalc.setOnClickListener {
-            if (calcDist.isChecked) dist()
-            if (calcTime.isChecked) time()
-            if (calcSpeed.isChecked) speed()
-        }
-        calcDist.setOnClickListener {
-            calc1.isVisible = true
-            calc2.isVisible = false
-            btnCalc.isVisible = true
-            tvCalcRes.isVisible = true
-        }
-        calcTime.setOnClickListener {
-            calc1.isVisible = false
-            calc2.isVisible = true
-            btnCalc.isVisible = true
-            tvCalcRes.isVisible = true
-        }
-        calcSpeed.setOnClickListener {
-            calc1.isVisible = true
-            calc2.isVisible = true
-            btnCalc.isVisible = true
-            tvCalcRes.isVisible = false
-        }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+        etCalcDistS.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (cbFixDist.isChecked && flagListener == 6) speed()
+                if (!cbFixDist.isChecked && flagListener == 6) dist()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+        etCalcKmh.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (cbFixDist.isChecked && flagListener == 7) speed()
+                if (!cbFixDist.isChecked && flagListener == 7) time()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
     }
 
     private fun mKmToKmH() {
@@ -209,7 +226,6 @@ class Fragment1 : Fragment() {
         }
 
         if (paceSec != kmSec) {
-            alert()
         } else distCalc()
     }
 
@@ -219,7 +235,6 @@ class Fragment1 : Fragment() {
             (((etCalcDistHour * hour) + etCalcDistMin) * hour) + etCalcDistSec.toDouble()
         val mSec = etKm / 3600
         val res = (distSec * mSec).toBigDecimal().setScale(3, RoundingMode.HALF_UP).toDouble()
-        tvCalcRes.text = "$res km"
         etCalcKmh.setText("$res")
     }
 
@@ -246,7 +261,6 @@ class Fragment1 : Fragment() {
             kmSec = (km / (etKm / second)).toInt()
         }
         if (paceSec != kmSec) {
-            alert()
         } else timeCalc()
     }
 
@@ -256,7 +270,6 @@ class Fragment1 : Fragment() {
         val hour2 = (etCalcKm / etKm).toInt()
         val min = ((sumSec - (hour2 * 3600)) / hour).toInt()
         val sec = (sumSec - (hour2 * 3600) - (min * hour)).toInt()
-        tvCalcRes.text = ("$hour2:$min:$sec")
         etCalcDistH.setText("$hour2")
         etCalcDistM.setText("$min")
         etCalcDistS.setText("$sec")
@@ -270,7 +283,10 @@ class Fragment1 : Fragment() {
         if (timeCalc == 0 || distCalc == 0.0) {
             etSpeed.setText("0.00")
             kmHToMKm()
-        } else speedCalc()
+        } else {
+            speedCalc()
+            kmHToMKm()
+        }
 
     }
 
@@ -281,26 +297,6 @@ class Fragment1 : Fragment() {
         val mSec = distM / distSec
         res = ((mSec * 3600) / km).toBigDecimal().setScale(2, RoundingMode.HALF_UP).toDouble()
         etSpeed.setText("$res")
-        kmHToMKm()
-    }
-
-    private fun alert() {
-        val builder = AlertDialog.Builder(activity!!)
-        val message = "Calculate value by min/km ($etMin:$etSec) or km/h ($etKm)"
-        builder.setTitle(R.string.alertTitle)
-        builder.setMessage(message)
-        builder.setNegativeButton("$etMin:$etSec") { _, _ ->
-            mKmToKmH()
-            if (calcDist.isChecked) dist()
-            if (calcTime.isChecked) time()
-        }
-        builder.setPositiveButton("$etKm") { _, _ ->
-            kmHToMKm()
-            if (calcDist.isChecked) dist()
-            if (calcTime.isChecked) time()
-        }
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
     }
 
     private fun notNull() {
@@ -310,42 +306,17 @@ class Fragment1 : Fragment() {
     }
 
     private fun notNullCalc() {
-        if (calcDist.isChecked) {
-            etCalcDistHour =
-                if (etCalcDistH.text.toString().isEmpty()) 0 else etCalcDistH.text.toString().toInt()
-            etCalcDistMin =
-                if (etCalcDistM.text.toString().isEmpty()) 0 else etCalcDistM.text.toString().toInt()
-            etCalcDistSec =
-                if (etCalcDistS.text.toString().isEmpty()) 0 else etCalcDistS.text.toString().toInt()
-            if (etCalcDistHour == 0) etCalcDistH.setText("0")
-            if (etCalcDistMin == 0) etCalcDistM.setText("0")
-            if (etCalcDistSec == 0) etCalcDistS.setText("0")
-            if (etCalcDistHour < 10) etCalcDistH.setText("0$etCalcDistHour")
-            if (etCalcDistMin < 10) etCalcDistM.setText("0$etCalcDistMin")
-            if (etCalcDistSec < 10) etCalcDistS.setText("0$etCalcDistSec")
-        }
-        if (calcTime.isChecked) {
-            etCalcKm =
-                if (etCalcKmh.text.toString().isEmpty()) 0.0 else etCalcKmh.text.toString().toDouble()
-            if (etCalcKm == 0.0) etCalcKmh.setText("0.00")
-        }
-        if (calcSpeed.isChecked) {
-            etCalcDistHour =
-                if (etCalcDistH.text.toString().isEmpty()) 0 else etCalcDistH.text.toString().toInt()
-            etCalcDistMin =
-                if (etCalcDistM.text.toString().isEmpty()) 0 else etCalcDistM.text.toString().toInt()
-            etCalcDistSec =
-                if (etCalcDistS.text.toString().isEmpty()) 0 else etCalcDistS.text.toString().toInt()
-            if (etCalcDistHour == 0) etCalcDistH.setText("0")
-            if (etCalcDistMin == 0) etCalcDistM.setText("0")
-            if (etCalcDistSec == 0) etCalcDistS.setText("0")
-            if (etCalcDistHour < 10) etCalcDistH.setText("0$etCalcDistHour")
-            if (etCalcDistMin < 10) etCalcDistM.setText("0$etCalcDistMin")
-            if (etCalcDistSec < 10) etCalcDistS.setText("0$etCalcDistSec")
-            etCalcKm =
-                if (etCalcKmh.text.toString().isEmpty()) 0.0 else etCalcKmh.text.toString().toDouble()
-            if (etCalcKm == 0.0) etCalcKmh.setText("0.00")
-        }
+
+        etCalcDistHour =
+            if (etCalcDistH.text.toString().isEmpty()) 0 else etCalcDistH.text.toString().toInt()
+        etCalcDistMin =
+            if (etCalcDistM.text.toString().isEmpty()) 0 else etCalcDistM.text.toString().toInt()
+        etCalcDistSec =
+            if (etCalcDistS.text.toString().isEmpty()) 0 else etCalcDistS.text.toString().toInt()
+        etCalcKm =
+            if (etCalcKmh.text.toString().isEmpty()) 0.0 else etCalcKmh.text.toString().toDouble()
+
     }
 }
+
 
