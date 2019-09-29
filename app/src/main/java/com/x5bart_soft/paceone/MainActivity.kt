90a5df3288b71efe0e2ea510b4ee27f857d99cea
Pavel Kovalchuk <x5bart@yandex.ru>
2019-09-29 10:00:53 +0300

    add ActionBar

--- app/src/main/java/com/x5bart_soft/paceone/MainActivity.kt
+++ app/src/main/java/com/x5bart_soft/paceone/MainActivity.kt
@@ -3,13 +3,20 @@ package com.x5bart_soft.paceone
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val frag1 = Fragment1()
+        val ft = supportFragmentManager.beginTransaction()
+        ft.add(R.id.frgCont, frag1)
+        ft.commit()
 
     }
+
 
 }
