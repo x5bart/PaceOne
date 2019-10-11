5b38cecd08a8933a1bab52ced3225cf0cd6849e3
Pavel Kovalchuk <p.kovalchuk@bestoptom.ru>
2019-10-11 12:19:38 +0300

    add ADFOX

--- app/src/main/java/com/x5bart_soft/paceone/MainActivity.kt
+++ app/src/main/java/com/x5bart_soft/paceone/MainActivity.kt
@@ -5,9 +5,19 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.yandex.mobile.ads.AdEventListener
+import com.yandex.mobile.ads.AdRequest
+import com.yandex.mobile.ads.AdSize
+import com.yandex.mobile.ads.AdView
+
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
+    private val BLOCK_ID = "adf-326819/1042468"
+    private var mAdView: AdView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +30,34 @@ class MainActivity : AppCompatActivity() {
         val ft = supportFragmentManager.beginTransaction()
         ft.add(R.id.frgCont, frag1)
         ft.commit()
-    }
+
+        mAdView = findViewById(R.id.banner_view)
+        mAdView!!.blockId = BLOCK_ID
+        mAdView!!.adSize = AdSize.BANNER_320x50
+
+        val parameters = HashMap<String, String>()
+        parameters["adf_ownerid"] = "example"
+        parameters["adf_p1"] = "example"
+        parameters["adf_p2"] = "example"
+        parameters["adf_pfc"] = "example"
+        parameters["adf_pfb"] = "example"
+        parameters["adf_plp"] = "example"
+        parameters["adf_pli"] = "example"
+        parameters["adf_pop"] = "example"
+        parameters["adf_pt"] = "example"
+        parameters["adf_pd"] = "example"
+        parameters["adf_pw"] = "example"
+        parameters["adf_pv"] = "example"
+        parameters["adf_prr"] = "example"
+        parameters["adf_pdw"] = "example"
+        parameters["adf_pdh"] = "example"
+        parameters["adf_puid1"] = "example"
+
+        val adRequest = AdRequest.builder().withParameters(parameters).build()
+
+        mAdView!!.loadAd(adRequest)
+        }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
@@ -37,4 +74,7 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
+
 }
