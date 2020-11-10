e08b503059576ff9fc0b72449d82dfe1c2bd48e6
Pavel Kovalchuk <x5bart@yandex.ru>
2020-11-10 15:07:55 +0300

    add splashScreen

---
+++ app/src/main/java/com/x5bart_soft/paceone/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.x5bart_soft.paceone
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
